279af7db694d1f1a0a956931125d3772893e0e1b
AnkitaPatil <103315919+AnkitaPatil2002@users.noreply.github.com>
2023-07-11 11:31:10 +0530

    Update SolidRectangle.cpp
    input: 3
    output:
    * * *
    * * *
    * * *

--- SolidRectangle.cpp
+++ SolidRectangle.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+void singleRectangle(int n ){
+  for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+              cout<<"*"<<" ";
+        }
+        cout<<endl;
+  }
+}
+
+int main(){
+  int n;
+  cin>>"Enter the value">>n;
+  singleRectangle(n);
+  return 0;
+}
